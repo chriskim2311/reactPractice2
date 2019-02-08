a1eaa43e1a5bff0352a6a30f6e72133c8246b923
chriskim2311 <chriskim2311@gmail.com>
2019-02-07 22:42:02 -0800

    Finished react weather season mini app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
-import App from './components/app';
+// import App from './components/app';
+
+
+// const App = () => {
+
+//     window.navigator.geolocation.getCurrentPosition(
+//         position =>console.log(position),
+//         err => console.log(err)
+//     )
+
+//     return <div>hi there</div>
+// }
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            lat: null,
+            errorMessage: ''
+
+        }
+    }
+
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position =>  this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        )
+
+    }
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return(
+                <div>Error: {this.state.errorMessage}</div>
+            )
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return(
+                <SeasonDisplay lat= {this.state.lat} />
+            )
+        }
+        return (
+       <Spinner message= "Please accept location request"/>
+
+
+
+
+        )
+
+    }
+
+    render() {
+return(
+    <div className="border red">
+    
+    {this.renderContent()}
+    
+    </div>
+)
+    }
+
+
+}
+
 
 ReactDOM.render(
     <App />,
